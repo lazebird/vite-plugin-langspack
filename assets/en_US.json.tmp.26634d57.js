b5afea28e3c75497920282c8dc8aa615c30e01e2
lazebird <lazebird@163.com>
2022-04-07 11:23:04 +0000

    Deploying to gh-pages from @ lazebird/vite-plugin-langspack@2dd316dc7fde9da731a32e99cf176e250b480d46 🚀

---
+++ assets/en_US.json.tmp.26634d57.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index.c4069385.js";const s=async a=>(await t.get(a)).data;var o=s("/en_US.json").then(a=>a);export{o as default};
--- assets/zh_CN.json.tmp.b6ea1975.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index.c4069385.js";const s=async a=>{const t=await o.get(a);return console.log(t.data),t.data};var e=s("/zh_CN.json").then(a=>a);export{e as default};
