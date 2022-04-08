a890d5b57dbed1aa5a7a11e9c3193dc43c881d77
lazebird@163.com <>
2022-04-08 11:11:14 +0800

    fix: remove unused files

--- langs/en_US.ts
+++
@@ -1,4 +0,0 @@
-import { genMessage } from '../src/locales/helper';
-
-const modules = import.meta.globEager('./en_US/**/*.json');
-export default { ...genMessage(modules, 'en_US') };
--- langs/zh_CN.ts
+++
@@ -1,4 +0,0 @@
-import { genMessage } from '../src/locales/helper';
-
-const modules = import.meta.globEager('./zh_CN/**/*.json');
-export default { ...genMessage(modules, 'zh_CN') };
