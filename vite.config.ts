4aa2730998122adc798542c43780d1c06b212fd9
lazebird <lazebird@163.com>
2023-04-04 10:02:11 +0800

    fix: use default target names, update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig } from 'vite';
+import { PluginOption, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import Components from 'unplugin-vue-components/vite';
 import vitePluginLangspack from './src/components/langspack/index';
 
 import visualizer from 'rollup-plugin-visualizer';
-const plugins = [];
-if (process.env.REPORT === 'true') plugins.push(visualizer({ open: true, gzipSize: true, brotliSize: true }));
+const plugins: PluginOption[] = [];
 
-function pathResolve(dir: string) {
-  return resolve(process.cwd(), '.', dir);
-}
+if (process.env.REPORT === 'true') plugins.push(visualizer({ open: true, gzipSize: true, brotliSize: true }));
+const pathResolve = (dir: string) => resolve(process.cwd(), '.', dir);
 
 const buildlib = {
   lib: {
     entry: pathResolve('src/components/langspack/index.ts'),
     name: 'vite-plugin-langspack',
-    fileName: (format) => `vite-plugin-langspack.${format}.js`,
   },
   sourcemap: true,
   rollupOptions: {
@@ -27,13 +24,7 @@ const buildlib = {
 const builddemo = {};
 
 export default defineConfig(({ command, mode }) => {
-  if (mode !== 'build')
-    plugins.push(
-      vitePluginLangspack({
-        lang: { src: pathResolve('langs'), dst: pathResolve('public'), include: /.json$/ },
-        log: { level: 6, filters: ['/node_modules/'], maxLen: 200 },
-      }),
-    );
+  if (mode !== 'build') plugins.push(vitePluginLangspack({ lang: { src: pathResolve('langs'), dst: pathResolve('public'), include: /.json$/ }, log: { level: 6, filters: ['/node_modules/'], maxLen: 200 } }));
   console.log('[vite.config.ts] command %s, mode %s, plugins %s', command, mode, JSON.stringify(plugins));
   return {
     base: './',
